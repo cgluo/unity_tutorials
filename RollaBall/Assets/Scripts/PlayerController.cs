50c17887d5e1e4924cbbe386d4bf746f6bd6a893
JackieLuo <cg_luo@163.com>
2017-04-14 17:06:12 +0800

    Roll a Ball - Collecting the Pick Up Objects

--- RollaBall/Assets/Scripts/PlayerController.cs
+++ RollaBall/Assets/Scripts/PlayerController.cs
@@ -1,1 +1,7 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class PlayerController : MonoBehaviour {    public float speed;    private Rigidbody rb;    void Start()    {        rb = GetComponent<Rigidbody>();    }    void FixedUpdate()    {        float moveHorizontal = Input.GetAxis("Horizontal");        float moveVertical = Input.GetAxis("Vertical");        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);        rb.AddForce(movement * speed);    }}
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class PlayerController : MonoBehaviour {    public float speed;    private Rigidbody rb;    void Start()    {        rb = GetComponent<Rigidbody>();    }    void FixedUpdate()    {        float moveHorizontal = Input.GetAxis("Horizontal");        float moveVertical = Input.GetAxis("Vertical");        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);        rb.AddForce(movement * speed);    }    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }}
